4d10863e4ae4b6b7d99d3e6b47e172f892ec7a62
Gomdori321 <102220858+Gomdori321@users.noreply.github.com>
2022-09-26 12:12:42 +0900

    Add files via upload

---
+++ 새 폴더 (4)/새 폴더 (2)/sssss.js
@@ -0,0 +1,27 @@
+let img = document.getElementById('abcd')
+let years = prompt('년도를 입력해주세요')
+
+if(years%12 == 0)
+    img.src = 'https://en.pimg.jp/071/758/381/1/71758381.jpg';
+else if(years%12 == 1)
+        img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCTQLh1Z2BQth-e5_K847eqQQkIToJHM5SWyODft0&s';
+else    if(years%12 == 2)
+        img.src = 'https://i.pinimg.com/236x/b5/56/fd/b556fdf429d8de25c3acf62f8186ddb9.jpg';
+   else if(years%12 == 3)
+        img.src = 'https://i.pinimg.com/550x/0b/7d/11/0b7d11c57fcff19628bcca20a5ad8fbd.jpg';
+  else  if(years%12 == 4)
+        img.src = 'http://www.boians.com/wp-content/uploads/2017/11/Boians_Vector_Twelve_Asian_Zodiac_Character_SKU_KOREA001967.jpg';
+   else if(years%12 == 5)
+        img.src = 'https://i.pinimg.com/550x/64/76/fc/6476fcfd9b06e1f5d16b0702687392bf.jpg';
+  else  if(years%12 == 6)
+        img.src = 'https://img.freepik.com/premium-vector/cute-funny-baby-tiger-with-flower-character-vector-hand-drawn-cartoon-kawaii-character-illustration-icon-isolated-on-white-background-cute-tiger-on-grass-cartoon-mascot-concept_92289-3064.jpg';
+  else  if(years%12 == 7)
+        img.src = 'https://en.pimg.jp/070/251/277/1/70251277.jpg';
+  else  if(years%12 == 8)
+        img.src = 'https://img.freepik.com/premium-vector/cute-dragon-cartoon-character-design_482416-180.jpg'
+ else   if(years%12 == 9)
+        img.src = 'https://us.123rf.com/450wm/natchapohn/natchapohn1611/natchapohn161100166/66853196-%EB%85%B9%EC%83%89-%EB%B1%80-%EB%8F%99%EB%AC%BC-%EB%A7%8C%ED%99%94-%EC%BA%90%EB%A6%AD%ED%84%B0%EC%9E%85%EB%8B%88%EB%8B%A4-%ED%9D%B0%EC%83%89-%EB%B0%B0%EA%B2%BD%EC%97%90-%EA%B3%A0%EB%A6%BD-%EC%82%BD%ED%99%94-.jpg'
+  else  if(years%12 == 10)
+        img.src = 'https://img.freepik.com/premium-vector/cute-horse-character-design_123847-659.jpg'
+  else  if(years%12 == 11)
+        img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWciAx9Dyr19MMsvzZWVggmm2lwT50TEFos9ZuizM&s'
---
+++ 새 폴더 (4)/새 폴더 (5)/randomcolor.js
@@ -0,0 +1,8 @@
+let randomBrightColor = () => {
+    let color_r = Math.floor(Math.random() * 127 + 128).toString(16);
+    let color_g = Math.floor(Math.random() * 127 + 128).toString(16);
+    let color_b = Math.floor(Math.random() * 127 + 128).toString(16);
+    return `#${color_r+color_g+color_b}`;
+  }
+  
+//background-color : randomBrightColor();
